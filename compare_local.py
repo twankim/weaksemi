5d3bc5d65cc6c45fc5d295710bce32938a1d6730
twankim <twankim@utexas.edu>
2017-10-22 02:37:46 -0500

    Testing Parameters

--- compare_local.py
+++ compare_local.py
@@ -2,7 +2,7 @@
 # @Author: twankim
 # @Date:   2017-02-24 17:46:51
 # @Last Modified by:   twankim
-# @Last Modified time: 2017-10-22 02:27:57
+# @Last Modified time: 2017-10-22 02:37:32
 
 import numpy as np
 import time
@@ -163,7 +163,7 @@ def parse_args():
                         default = 2, type = int)
     parser.add_argument('-std', dest='std',
                         help='standard deviation of Gaussian distribution (default:1.5)',
-                        default = 2.0, type = float)
+                        default = 2.25, type = float)
     parser.add_argument('-qs', dest='qs',
                         help='Probabilities q (not-sure with 1-q) ex) 0.7,0.85,1',
                         default = '0.7,0.85,1', type = str)
@@ -178,7 +178,7 @@ def parse_args():
                         default = 1.0, type = float)
     parser.add_argument('-g_max', dest='max_gamma',
                         help='minimum gamma margin (default:1)',
-                        default = 1.1, type = float)
+                        default = 1.05, type = float)
     parser.add_argument('-cs', dest='cs',
                         help='Fractions to set distance-weak parameters (0.5,1] ex) 0.7,0.85,1',
                         default = '0.6,0.8,1', type = str)
